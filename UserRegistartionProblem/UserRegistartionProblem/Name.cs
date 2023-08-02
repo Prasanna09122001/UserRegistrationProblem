62244a4054b80f9c621d23626ebc4fa1a830bf97
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-08-02 13:31:13 +0530

    [Prasanna] Add:Implement the Check of First Name

---
+++ UserRegistartionProblem/UserRegistartionProblem/Name.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistartionProblem
+{
+    internal class Name
+    {
+        string name_Regex = "^[A-Z]{1}[a-z]{2,}";
+        public void name(string name)
+        {
+            bool result = Regex.IsMatch(name, name_Regex);
+            if (result)
+                Console.WriteLine("The Given Name " + name + " is in Correct Format");
+            else
+                Console.WriteLine("The Given Name " + name + " is Not in Correct Format");
+        }
+    }
+}
--- UserRegistartionProblem/UserRegistartionProblem/Program.cs
+++ UserRegistartionProblem/UserRegistartionProblem/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
-    namespace UserRegistationProblem
+using UserRegistartionProblem;
+
+namespace UserRegistationProblem
 {
     class program
     {
         static void Main()
         {
             Console.WriteLine("Welcome to the User Registation Problem");
+            Console.WriteLine("Enter Your Name with First Letter Capital");
+            string name = Console.ReadLine();
+            Name name1 = new Name();
+            name1.name(name);
+
         }
     }
 }
