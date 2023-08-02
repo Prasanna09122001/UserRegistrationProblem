9f4aab53a7ae210feb1288773bbf1eb6a21169d3
Prasanna Venkatesh R P <prasannavenkatesh.rp@tvsnext.com>
2023-08-02 13:19:18 +0530

    [Prasanna] Add: Strcuture Folder Created

---
+++ UserRegistartionProblem/UserRegistartionProblem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+    namespace UserRegistationProblem
+{
+    class program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to the User Registation Problem");
+        }
+    }
+}
+
